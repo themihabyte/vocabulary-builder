ff8666b50dce7848781932eb88c94c7c0d3cb650
themihabyte <mihabayt@gmail.com>
2025-09-27 12:59:23 +0200

    Added Atomic remove, upsert, watch

--- lib/services/abstract_deck_repository.dart
+++ lib/services/abstract_deck_repository.dart
@@ -3,4 +3,7 @@ import '../models/card_model.dart';
 abstract class AbstractDeckRepository {
   Future<List<CardModel>> loadDeck();
   Future<void> saveDeck(List<CardModel> cards);
+  Stream<List<CardModel>> watchDeck();
+  Future<void> upsertCard(CardModel card);
+  Future<void> removeCard(String id);
 }
