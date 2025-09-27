1656461f3ba387bba0bdd24bd9681536174431b4
themihabyte <mihabayt@gmail.com>
2025-09-27 12:59:57 +0200

    Mutable and Immutable versions. Added id

--- lib/models/card_model.dart
+++ lib/models/card_model.dart
@@ -1,26 +1,62 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+
 import '../algorithms/abstract_scheduling_data.dart';
 import '../algorithms/probabilistic/probabilistic_scheduling_data.dart';
 
-class CardModel {
+const _uuid = Uuid();
+
+typedef Json = Map<String, Object?>;
+
+mixin CardFields {
+  String get id;
+  String get word;
+  String get translation;
+  String get exampleContext;
+  SchedulingData get schedulingData;
+  bool get isActive;
+}
+
+@immutable
+class CardModel with CardFields {
+  @override
+  final String id;
+  @override
   final String word;
+  @override
   final String translation;
+  @override
   final String exampleContext;
+  @override
   final SchedulingData schedulingData;
+  @override
   final bool isActive;
 
   CardModel({
+    String? id,
     required this.word,
     required this.translation,
     required this.exampleContext,
     required this.schedulingData,
     this.isActive = true,
-  });
+  }) : id = id ?? _uuid.v4();
 
-  Map<String, dynamic> toJson() => {
+  factory CardModel.from(CardFields other) => CardModel(
+        id: other.id,
+        word: other.word,
+        translation: other.translation,
+        exampleContext: other.exampleContext,
+        schedulingData: other.schedulingData,
+        isActive: other.isActive,
+      );
+
+  Json toJson() => <String, Object?>{
+        'id': id,
         'word': word,
         'translation': translation,
         'example_context': exampleContext,
-        'scheduling_data': schedulingData.toJson(),
+        'scheduling_data': schedulingData.toJson(), // should include "type"
         'is_active': isActive,
       };
 
@@ -44,15 +80,69 @@ class CardModel {
       schedulingData = ProbabilisticSchedulingData();
     }
     return CardModel(
+      id: json['id'] as String? ?? _uuid.v4(),
       word: json['word'] as String,
       translation: json['translation'] as String,
       exampleContext: json['example_context'] as String,
       schedulingData: schedulingData,
-      isActive: json['is_active'] as bool? ?? true,
+      isActive: (json['is_active'] as bool?) ?? true,
     );
   }
   @override
   String toString() {
     return 'CardModel(word: $word, translation: $translation, exampleContext: $exampleContext, schedulingData: $schedulingData, isActive: $isActive)';
   }
+}
+
+/// --- Mutable (for forms/editors) -------------------------------------------
+class MutableCardModel with CardFields {
+  @override
+  String id;
+  @override
+  String word;
+  @override
+  String translation;
+  @override
+  String exampleContext;
+  @override
+  SchedulingData schedulingData;
+  @override
+  bool isActive;
+
+  MutableCardModel({
+    String? id,
+    required this.word,
+    required this.translation,
+    required this.exampleContext,
+    required this.schedulingData,
+    this.isActive = true,
+  }) : id = id ?? _uuid.v4();
+
+  factory MutableCardModel.from(CardFields other) => MutableCardModel(
+        id: other.id,
+        word: other.word,
+        translation: other.translation,
+        exampleContext: other.exampleContext,
+        schedulingData: other.schedulingData,
+        isActive: other.isActive,
+      );
+}
+
+/// --- Tiny conversion helpers -----------------------------------------------
+extension CardModelX on CardModel {
+  MutableCardModel toMutable() => MutableCardModel.from(this);
+}
+
+extension MutableCardModelX on MutableCardModel {
+  CardModel toImmutable() => CardModel.from(this);
+}
+
+extension CardModelListX on List<CardModel> {
+  List<MutableCardModel> toMutableList() =>
+      map((c) => c.toMutable()).toList(growable: true);
+}
+
+extension MutableCardModelListX on List<MutableCardModel> {
+  List<CardModel> toImmutableList() =>
+      map((c) => c.toImmutable()).toList(growable: false);
 }
